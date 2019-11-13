43256c37febb7383cbda0abfc52a2c8e4ebd6519
Boris Gorbylev <ekho@ekho.name>
2019-11-13 10:34:52 +0300

    Fontawesome

--- assets/javascripts/subtask_tree_table.js
+++ assets/javascripts/subtask_tree_table.js
@@ -12,6 +12,11 @@
       getClasses(element, c => c.startsWith('idnt-')).forEach(cls => element.removeClass(cls));
     }
   });
+  
+  let classExpanded = 'fa-angle-down';
+  let classCollapsed = 'fa-angle-right';
+  let showAsExpanded = element => $(element).removeClass(classCollapsed).addClass(classExpanded);
+  let showAsCollapsed = element => $(element).removeClass(classExpanded).addClass(classCollapsed);
 
   $('div#issue_tree table.list.issues').simpleTreeTable({
     expander: '.subtask_all_expand',
@@ -19,5 +24,9 @@
     store: 'session', // or 'local'
     storeKey: 'KEY',
     iconPosition: '.subject',
-  });
+    iconTemplate: '<span class="rm-icon fa-fw"></span>'
+  }).on('node:open',(e, $node) => showAsExpanded($($node).find('.simple-tree-table-icon'))).on('node:close', (e, $node) => showAsCollapsed($($node).find('.simple-tree-table-icon')));
+
+  showAsExpanded('.simple-tree-table-opened .simple-tree-table-icon');
+  showAsCollapsed('.simple-tree-table-closed .simple-tree-table-icon');
 })})(jQuery);
