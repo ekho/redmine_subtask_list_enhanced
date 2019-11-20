909dca03b8b1067e8e268bf2d66126d28cb277cb
Boris Gorbylev <ekho@ekho.name>
2019-11-20 19:47:43 +0300

    Fixed IssuesHelperPatch

--- lib/redmine_subtask_list_enhanced/patches/issues_helper_patch.rb
+++ lib/redmine_subtask_list_enhanced/patches/issues_helper_patch.rb
@@ -3,11 +3,22 @@ require_dependency("issues_helper")
 module RedmineSubtaskListEnhanced
   module Patches
     module IssuesHelperPatch
-      extend ActiveSupport::Concern
+      def self.included(base) # :nodoc:
+        base.send(:include, InstanceMethods)
+        base.class_eval do
+          unloadable
+        end
+      end
 
-      def sla_has_grandson_issues?(issue)
-        return issue.descendants.visible.where(["issues.parent_id <> ?", issue.id]).count > 0
+      module InstanceMethods
+        def sla_has_grandson_issues?(issue)
+          issue.descendants.visible.where(["issues.parent_id <> ?", issue.id]).count > 0
+        end
       end
     end
   end
+end
+
+unless IssuesHelper.included_modules.include?(RedmineSubtaskListEnhanced::Patches::IssuesHelperPatch)
+  IssuesHelper.send(:include, RedmineSubtaskListEnhanced::Patches::IssuesHelperPatch)
 end
