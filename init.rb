e5c3d41d96eff2e4ae38936036f2e714842808e0
root <root@rdbl.contronix.de>
2023-01-05 07:39:55 +0000

    redmine 5.0 compatibility

--- init.rb
+++ init.rb
@@ -1,5 +1,3 @@
-require_dependency 'redmine_subtask_list_enhanced/hooks/subtask_list_enhanced_hook'
-
 Redmine::Plugin.register :redmine_subtask_list_enhanced do
   name 'Redmine Subtask List Enhanced plugin'
   author 'Boris Gorbylev'
@@ -7,4 +5,13 @@ Redmine::Plugin.register :redmine_subtask_list_enhanced do
   version '1.1.1'
   url 'https://github.com/ekho/redmine_subtask_list_enhanced'
   author_url 'https://github.com/ekho'
+end
+
+if Rails.version > '6.0'
+  if Rails.configuration.respond_to?(:autoloader) && Rails.configuration.autoloader == :zeitwerk
+    Rails.autoloaders.each { |loader| loader.ignore(File.dirname(__FILE__) + '/lib') }
+  end
+  require_dependency File.dirname(__FILE__) + '/lib/redmine_subtask_list_enhanced/hooks/subtask_list_enhanced_hook'
+else
+  require_dependency 'redmine_subtask_list_enhanced/hooks/subtask_list_enhanced_hook'
 end
